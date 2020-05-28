a724c2609b1d5fa9fcbd893da5ad873f8942776b
Kalan <iios_developer@sina.com>
2020-05-28 09:08:55 +0800

    Initial commit

---
+++ Example/KLGuidePage/KLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLAppDelegate.h
+//  KLGuidePage
+//
+//  Created by Kalanhall@163.com on 05/28/2020.
+//  Copyright (c) 2020 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/KLGuidePage/KLViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLViewController.h
+//  KLGuidePage
+//
+//  Created by Kalanhall@163.com on 05/28/2020.
+//  Copyright (c) 2020 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLViewController : UIViewController
+
+@end
